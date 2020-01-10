5c54b6d1494ccfc56a04f262eb126011a81f6450
Cosmic0Strom <arvid.rohrmann@gmx.de>
2020-01-10 22:06:25 +0100

    Config changed

--- Data/config.py
+++ Data/config.py
@@ -5,7 +5,6 @@ class Iserv:
     paswd=""
 class Mysql:
     user=""
-    paswd=""#<- needs lookup
-class Check:
-    host="127.0.0.1"
-    port=660
+    paswd=""
+    database=""
+    host=""#<- needs lookup
