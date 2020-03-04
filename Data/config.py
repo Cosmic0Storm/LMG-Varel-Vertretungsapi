ad9eddb26104c043e0c717e802f38a326ae62313
Cosmic0Storm <51029830+Cosmic0Storm@users.noreply.github.com>
2020-03-04 22:45:35 +0100

    reset config.py

--- Data/config.py
+++ Data/config.py
@@ -1,10 +1,10 @@
 #Config Datei  mit Login Daten
 
 class Iserv:
-    user="arvid.rohrmann"
-    paswd="Flitzer-123"
+    user=""
+    paswd=""
 class Mysql:
-    user="iserv_user"
-    paswd="k2BL2H4kZu5kMs"
-    database="iserv"
-    host="localhost"#<- needs lookup
+    user=""
+    paswd=""
+    database=""
+    host=""#<- needs lookup
