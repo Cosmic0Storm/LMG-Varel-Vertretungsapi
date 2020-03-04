d3f97c0925ccce8b99da22beba9bafc179a38c02
Cosmic0Storm <arvid.rohrmann@gmx.de>
2020-03-04 19:08:09 +0100

    Fix DB Model & Rearange command line interface

--- Data/Database.py
+++ Data/Database.py
@@ -15,17 +15,17 @@ class Database:
     def set(self,Data):
         if not isinstance(Data,Data.__class__):
             return
-        self.cursor.execute("show tables")
-        if (Data.Name,) not in self.cursor.fetchall():
-            self.cursor.execute('''CREATE TABLE  if not exists`{0}` (   `Date` tinytext,`Klasse` tinytext,   `Stunde` tinytext,   `Vertreter` tinytext,   `Fach` tinytext,   `Raum` tinytext,   `Entfallener_Lehrer` tinytext,   `Entfallenes_Fach` tinytext) ENGINE=InnoDB DEFAULT CHARSET=utf8;'''.format(Data.Name))
         for a in range(len(Data.Table)):
-            self.cursor.execute("""UPDATE `{name}` SET `Date`='{date}',`Vertreter`='{2}',`Fach`='{3}',`Raum`='{4}',`Entfallener_Lehrer`='{5}',`Entfallenes_Fach`='{6}' WHERE `Klasse`='{0}' AND `Stunde`='{1}'; """.format(*Data.Table[a],name=Data.Name,date=Data.Date))
-            self.cursor.execute("""SELECT COUNT(1) FROM `{name}` WHERE `Klasse`='{0}' and `Stunde`='{1}'""".format(*Data.Table[a],name=Data.Name))
+            self.cursor.execute("""UPDATE `Vertretungsplan` SET `Date`='{date}',`Vertreter`='{2}',`Fach`='{3}',`Raum`='{4}',`Entfallener_Lehrer`='{5}',`Entfallenes_Fach`='{6}' WHERE `Klasse`='{0}' AND `Stunde`='{1}'; """.format(*Data.Table[a],date=Data.Name))
+            self.cursor.execute("""SELECT COUNT(1) FROM `Vertretungsplan` WHERE `Klasse`='{0}' and `Stunde`='{1}' and `Date`='{date}'""".format(*Data.Table[a],Data.Name))
             if  self.cursor.fetchall()[0][0]==0:
-                self.cursor.execute('''INSERT INTO `{name}` VALUES('{0}','{1}','{2}','{3}','{4}','{5}','{6}');'''.format(*Data.Table[a],name=Data.Name))
-        self.cursor.execute("select * from `{name}`".format(name=Data.Name))
+                self.cursor.execute('''INSERT INTO `{name}` VALUES('{date}','{0}','{1}','{2}','{3}','{4}','{5}','{6}');'''.format(*Data.Table[a],Data.Name))
+        self.cursor.execute("select * from `Vertretungsplan`")
         Table=self.cursor.fetchall()
         for c in range(len(Table)):
             if list(Table[c]) not in Data.Table:
-                self.cursor.execute('''delete from `{name}` where `Klasse`='{0}' and `Fach`='{3}' and `Stunde`='{1}' '''.format(*Table[c],name=Data.Name))
+                self.cursor.execute('''delete from `Vertretungsplan` where `Klasse`='{0}' and `Fach`='{3}' and `Stunde`='{1}' and `Date`='{date}'  '''.format(*Table[c],date=Data.Name))
         self.sql.commit()
+
+    def setup(self):
+        self.cursor.execute('''CREATE TABLE  if not exists`Vertretungsplan` (   `Date` tinytext,`Klasse` tinytext,   `Stunde` tinytext,   `Vertreter` tinytext,   `Fach` tinytext,   `Raum` tinytext,   `Entfallener_Lehrer` tinytext,   `Entfallenes_Fach` tinytext) ENGINE=InnoDB DEFAULT CHARSET=utf8;'''.format(Data.Name))
--- Data/main.py
+++ Data/main.py
@@ -1,14 +1,22 @@
 import sys
 import update
-
-if sys.argv[1]=="-D":
-    if sys.argv[2]=="heute":
+from Database import Database
+if "-t" in sys.argv:
+    i = sys.argv.index("-t")
+    if sys.argv[i+1]=="heute":
         update.Update_Thread("heute").start()
-    elif sys.argv[3]=="morgen":
+    elif sys.argv[i+1]=="morgen":
         update.Update_Thread("morgen").start()
     else:
         print("Error: Wrong Argument")
+elif "-DB" in sys.argv:
+    i=sys.argv.index("-DB")
+    if sys.argv[i+1]=="setup":
+        D=Database()
+        D.setup()
+    else:
+        print("Only setup interface is available")
 else:
-    print("Error: No Argument \n -D {heute:morgen}")
+    print("Error: No Argument \n -t {heute:morgen}#define type of script \n -DB setup#Setup DB")
 
     
